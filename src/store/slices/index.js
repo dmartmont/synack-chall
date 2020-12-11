cee7f9a050c233ef008771fd212804b23270aa1f
Daniel Martinez <dmartinezmont@gmail.com>
2020-12-10 22:48:08 -0500

    Remove unnecessary files

--- src/store/slices/index.js
+++ src/store/slices/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import searchReducer from './search.slice';
 import searchResultReducer from './search-result.slice';
 
 export default combineReducers({
-  search: searchReducer,
   searchResult: searchResultReducer
 });
